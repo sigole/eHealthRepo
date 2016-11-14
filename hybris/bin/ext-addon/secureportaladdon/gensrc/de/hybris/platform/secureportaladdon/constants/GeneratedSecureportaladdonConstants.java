8b40ac90bbab40c2c38369f794ae2f3480c7d37f
sigole <siddhesh.gole@capgemini.com>
2016-11-14 14:16:18 +0530

    secureportaladdon folder added

---
+++ hybris/bin/ext-addon/secureportaladdon/gensrc/de/hybris/platform/secureportaladdon/constants/GeneratedSecureportaladdonConstants.java
@@ -0,0 +1,51 @@
+/*
+ * ----------------------------------------------------------------
+ * --- WARNING: THIS FILE IS GENERATED AND WILL BE OVERWRITTEN! ---
+ * --- Generated at Nov 11, 2016 1:09:19 PM                     ---
+ * ----------------------------------------------------------------
+ *  
+ * [y] hybris Platform
+ *  
+ * Copyright (c) 2000-2016 SAP SE
+ * All rights reserved.
+ *  
+ * This software is the confidential and proprietary information of SAP
+ * Hybris ("Confidential Information"). You shall not disclose such
+ * Confidential Information and shall use it only in accordance with the
+ * terms of the license agreement you entered into with SAP Hybris.
+ *  
+ */
+package de.hybris.platform.secureportaladdon.constants;
+
+/**
+ * @deprecated use constants in Model classes instead
+ */
+@Deprecated
+@SuppressWarnings({"unused","cast","PMD"})
+public class GeneratedSecureportaladdonConstants
+{
+	public static final String EXTENSIONNAME = "secureportaladdon";
+	public static class TC
+	{
+		public static final String B2BREGISTRATION = "B2BRegistration".intern();
+		public static final String B2BREGISTRATIONAPPROVEDPROCESS = "B2BRegistrationApprovedProcess".intern();
+		public static final String B2BREGISTRATIONPROCESS = "B2BRegistrationProcess".intern();
+		public static final String B2BREGISTRATIONREJECTEDPROCESS = "B2BRegistrationRejectedProcess".intern();
+		public static final String CMSSECUREPORTALRESTRICTION = "CMSSecurePortalRestriction".intern();
+	}
+	public static class Attributes
+	{
+		public static class CMSSite
+		{
+			public static final String ENABLEREGISTRATION = "enableRegistration".intern();
+			public static final String REQUIRESAUTHENTICATION = "requiresAuthentication".intern();
+		}
+	}
+	
+	protected GeneratedSecureportaladdonConstants()
+	{
+		// private constructor
+	}
+	
+	
+}
